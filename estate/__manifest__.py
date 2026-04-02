b3bb681e6170061137e4c5a4b848d10f91ba4475
DonJosueSuarez <josuea.suarezs@gmail.com>
2026-04-02 12:28:42 -0500

    Final Capitulo 4

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
-    'name': 'estate',
+    'name': 'Real Estate',
     'data': [
         'security/ir.model.access.csv',
-    ]
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,3 +1,5 @@
+from dateutil.relativedelta import relativedelta
+
 from odoo import models, fields
 
 
@@ -8,13 +10,15 @@ class StateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Datetime("Date Availability", default=lambda self: fields.Datetime.now() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(string="Garden Orientation", selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    active = fields.Boolean(default=False)
+    state = fields.Selection(string="State", selection=[('new', 'New'),('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')], default='new')
